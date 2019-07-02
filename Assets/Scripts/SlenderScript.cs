80995770cbb025534d71181abe836055934fa97c
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-07-02 10:10:31 +0300

    add enemy

---
+++ Assets/Scripts/SlenderScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SlenderScript : MonoBehaviour
+{
+    // GameObject destination;
+    NavMeshAgent navMeshAgent;
+
+    public Transform target;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // destination = GameObject.Find("Player");
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(navMeshAgent.velocity != Vector3.zero){
+            print("hell");
+            animator.SetBool("idle", false);
+        } else {
+            animator.SetBool("idle", true);
+        }
+        // Sight();
+        NavMeshHit hit;
+        if (!navMeshAgent.Raycast(target.position, out hit)) {
+            SetDestination();
+        }
+    }
+
+    public void SetDestination()
+    {
+        // navMeshAgent.SetDestination(destination.transform.position);
+        navMeshAgent.SetDestination(target.position);
+    }
+
+}

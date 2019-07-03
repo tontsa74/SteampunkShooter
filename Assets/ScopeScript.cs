ab3fcf839885b0b58851408988b7a83c6bb136bf
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-07-03 11:05:40 +0300

    add scope

---
+++ Assets/ScopeScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeScript : MonoBehaviour
+{
+    public Animator animator;
+
+    bool isScoped = false;
+
+    public GameObject scopeOverlay;
+    public GameObject weaponCamera;
+    public Camera mainCamera;
+
+    public float scopedFOV = 15f;
+    private float normalFOV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2")) {
+            isScoped = !isScoped;
+            animator.SetBool("Scoped", isScoped);
+
+            // scopeOverlay.SetActive(isScoped);
+
+            if (isScoped) {
+                StartCoroutine(OnScoped());
+            } else {
+                OnUnScoped();
+            }
+        }
+    }
+
+    IEnumerator OnScoped() {
+        yield return new WaitForSeconds(0.15f);
+
+        scopeOverlay.SetActive(true);
+        weaponCamera.SetActive(false);
+        normalFOV = mainCamera.fieldOfView;
+        mainCamera.fieldOfView = scopedFOV;
+    }
+
+    void OnUnScoped() {
+        scopeOverlay.SetActive(false);
+        weaponCamera.SetActive(true);
+        mainCamera.fieldOfView = normalFOV;
+    }
+}

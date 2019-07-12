8bc23bf0e69c90770d2bf310e4bd7335a81bd6f6
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-07-12 08:41:53 +0300

    add melee robot

--- Assets/Scripts/RobotRailgunnerScript.cs
+++ Assets/Scripts/RobotRailgunnerScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class SlenderScript : MonoBehaviour
+public class RobotRailgunnerScript : MonoBehaviour
 {
     // GameObject destination;
     NavMeshAgent navMeshAgent;

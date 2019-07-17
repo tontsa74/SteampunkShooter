c44da8ec97682c6d24f864bc789842cb8e149dfe
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-07-17 09:44:34 +0300

    pause

---
+++ Assets/PauseScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Cursor"))
+        {
+            print("pa" + pausePanel.activeInHierarchy);
+            if (!pausePanel.activeInHierarchy)
+            {
+                PauseGame();
+            }
+            if (pausePanel.activeInHierarchy)
+            {
+                ContinueGame();
+            }
+        }
+    }
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+        print("PAUSEDDD");
+        //Disable scripts that still work while timescale is set to 0
+    }
+    private void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        //enable the scripts again
+    }
+}

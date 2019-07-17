4cc877742bbaec1f6bfcc941c7aa6d0c2dcddaf7
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-07-17 12:14:44 +0300

    vertical bobbing

--- Assets/Scripts/HeadBobber.cs
+++ Assets/Scripts/HeadBobber.cs
@@ -5,10 +5,16 @@ public class HeadBobber : MonoBehaviour
 {
 
     private float timer = 0.0f;
+    private float verticalTimer = 0.0f;
+
     public float bobbingSpeed = 0.06f;
     public float bobbingAmount = 0.05f;
-   // public float verticalBobbingAmount = 0.025f;
     public float midpoint = 2.0f;
+
+    // public float verticalBobbingAmount = 0.025f;
+    public float verticalBobbingAmount = 0.035f;
+    public float verticalBobbingSpeed = 0.035f;
+    private float verticalMidpoint = 0f;
     public float runSpeedMultiplier = 7f;
     public float runAmountMultiplier = 1.5f;
     public float walkSpeedMultiplier = 5f;
@@ -26,11 +32,14 @@ public class HeadBobber : MonoBehaviour
     void Update()
     {
         float waveslice = 0.0f;
+        float verticalWaveslice = 0.0f;
+
         float horizontal = 1;
         float vertical = 1;
         if (!isGrounded)
         {
             timer = 0.0f;
+            verticalTimer = 0.0f;
         }
         else
         {
@@ -39,6 +48,13 @@ public class HeadBobber : MonoBehaviour
             if (timer > Mathf.PI * 2)
             {
                 timer = timer - (Mathf.PI * 2);
+            }
+
+            verticalWaveslice = Mathf.Sin(verticalTimer);
+            verticalTimer = verticalTimer + verticalBobbingSpeed;
+            if (verticalTimer > Mathf.PI * 2)
+            {
+                verticalTimer = verticalTimer - (Mathf.PI * 2);
             }
         }
 
@@ -55,7 +71,21 @@ public class HeadBobber : MonoBehaviour
         {
             v3T.y = midpoint;
         }
+
+        if (verticalWaveslice != 0)
+        {
+            float VerticalTranslateChange = verticalWaveslice * verticalBobbingAmount;
+            float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+            totalAxes = Mathf.Clamp(totalAxes, 0.0f, 1.0f);
+            VerticalTranslateChange = totalAxes * VerticalTranslateChange;
+            v3T.x = verticalMidpoint + VerticalTranslateChange;
+        }
+        else
+        {
+            v3T.x = verticalMidpoint;
+        }
         transform.localPosition = v3T;
+
     }
 
     public void RunningBobbing()

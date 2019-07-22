fd5babf6492ce5e1a9c37b6c7d06fef69d182952
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-07-22 16:05:43 +0300

    add spawn point

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject[] enemys;
+
+    void Start()
+    {
+
+        Spawn();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Spawn()
+    {
+        foreach (GameObject enemy in enemys)
+        {
+            Instantiate(enemy, transform.position, Quaternion.identity, transform);
+        }
+    }
+}

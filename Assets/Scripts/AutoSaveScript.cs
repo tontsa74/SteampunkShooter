f33d6c919bacee5bd554d084bd952c769eb60ef3
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-07-29 09:51:01 +0300

    add cheat key g to jump between autosave positions

--- Assets/Scripts/AutoSaveScript.cs
+++ Assets/Scripts/AutoSaveScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AutoSaveScript : MonoBehaviour
 {
+    public Transform[] autoSavePositions;
+    private int destPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,23 @@ public class AutoSaveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonUp("Goto")) {
+            GotoNextPoint();
+        }
+    }
+        void GotoNextPoint() {
+        // Returns if no points have been set up
+        if (autoSavePositions.Length == 0) {
+            return;
+        }
+            
+
+        // Set the agent to go to the currently selected destination.
+        transform.position = autoSavePositions[destPosition].position;
+        transform.rotation = autoSavePositions[destPosition].rotation;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPosition = (destPosition + 1) % autoSavePositions.Length;
     }
 }
